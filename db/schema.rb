5c06704ccc48a4a5ff72617fad870776232f6ff1
JonZach <Zach@yourgreenpal.com>
2013-06-18 15:12:25 -0500

    added user_id to Appointments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130618194807) do
+ActiveRecord::Schema.define(:version => 20130618200933) do
 
   create_table "appointments", :force => true do |t|
     t.date     "date"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20130618194807) do
     t.string   "zip"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
